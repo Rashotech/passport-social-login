5e4d4869841801f166ab820f981ca118e7286bf0
Rashotech <rashotech@gmail.com>
2021-04-25 16:09:39 +0100

    .

--- models/user.js
+++ models/user.js
@@ -4,8 +4,8 @@ const UserSchema = new mongoose.Schema({
     sid: { type: String, unique: true },
     firstName: { type: String, trim: true},
     lastName: { type: String, trim: true},
-    phone: { type: String, trim: true, unique: true},
-    email: { type: String, required: true, trim: true, lowercase: true, unique: true},
+    phone: { type: String, trim: true, unique: true, sparse: true},
+    email: { type: String, required: true, trim: true, lowercase: true, unique: true, sparse: true},
     password: { type: String }
 });
 
